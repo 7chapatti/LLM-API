ea600f09902b32197521e087ec30da46c7d01d01
7chapatti <rg2006@live.co.uk>
2026-08-30 19:30:58 +0100

    Add health check endpoint to app

--- src/app.js
+++ src/app.js
@@ -2,5 +2,8 @@ const express = require('express');
 const triageRoute = require('./routes/triage');
 const app = express();
 app.use(express.json());
+app.get('/', (_req, res) => {
+  res.json({ status: 'ok', endpoint: 'POST /triage' });
+});
 app.use('/', triageRoute);
 module.exports = app;
